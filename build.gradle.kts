9506470a2a3145c5af932321ccff7568b7ae584d
aesperer <piyrw9754@gmail.com>
2023-05-01 11:08:29 +0900

    :tada: Begin a Hi v2

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version PluginVersion.SPRING_BOOT_VERSION
+	id("io.spring.dependency-management") version PluginVersion.DEPENDENCY_MANAGER_VERSION
+	kotlin("jvm") version PluginVersion.JVM_VERSION
+	kotlin("plugin.spring") version PluginVersion.SPRING_PLUGIN_VERSION
+	kotlin("plugin.jpa") version PluginVersion.JPA_PLUGIN_VERSION
+	kotlin("kapt") version PluginVersion.KAPT_VERSION
+}
+
+group = "team.msg"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	// spring starter
+	implementation(Dependencies.SPRING_JPA)
+	implementation(Dependencies.SPRING_SECURITY)
+	implementation(Dependencies.SPRING_VALIDATION)
+	implementation(Dependencies.SPRING_WEB)
+
+	testImplementation(Dependencies.SPRING_STARTER_TEST)
+	testImplementation(Dependencies.SPRING_SECURITY_TEST)
+
+	// kapt
+	kapt(Dependencies.CONFIG_PROCESSOR)
+
+	// kotlin
+	implementation(Dependencies.JACKSON_MODULE_KOTLIN)
+	implementation(Dependencies.KOTLIN_REFLECT)
+
+	// database
+	runtimeOnly(Dependencies.MYSQL_DATABASE)
+
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "11"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/kotlin/team/msg/hiv2/Hiv2Application.kt
@@ -0,0 +1,11 @@
+package team.msg.hiv2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Hiv2Application
+
+fun main(args: Array<String>) {
+	runApplication<Hiv2Application>(*args)
+}
