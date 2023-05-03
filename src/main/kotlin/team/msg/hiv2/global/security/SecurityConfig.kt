e2ce534d97cd011dd00a4a7181fd992bafaf69bc
aesperer <piyrw9754@gmail.com>
2023-05-03 14:21:07 +0900

    create :: security configuration

--- src/main/kotlin/team/msg/hiv2/global/security/SecurityConfig.kt
+++ src/main/kotlin/team/msg/hiv2/global/security/SecurityConfig.kt
@@ -1,4 +1,48 @@
 package team.msg.hiv2.global.security
 
-class SecurityConfig {
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+import team.msg.hiv2.global.config.FilterConfig
+import team.msg.hiv2.global.security.handler.CustomAccessDenied
+import team.msg.hiv2.global.security.handler.CustomAuthenticationEntryPoint
+import team.msg.hiv2.global.security.spi.JwtParserPort
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig(
+    private val jwtParserPort: JwtParserPort
+) {
+
+    @Bean
+    protected fun filterChain(http: HttpSecurity): SecurityFilterChain =
+        http
+            .cors()
+            .and()
+            .csrf().disable()
+            .httpBasic().disable()
+
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+
+            .authorizeRequests()
+            .anyRequest().authenticated()
+            .and()
+
+            .exceptionHandling()
+            .authenticationEntryPoint(CustomAuthenticationEntryPoint())
+            .accessDeniedHandler(CustomAccessDenied())
+            .and()
+
+            .apply(FilterConfig(jwtParserPort))
+            .and()
+            .build()
+
+    @Bean
+    protected fun passwordEncoder() = BCryptPasswordEncoder()
 }
