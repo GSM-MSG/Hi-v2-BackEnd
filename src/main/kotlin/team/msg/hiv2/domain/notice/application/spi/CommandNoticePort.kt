560587f0e08834803af18032c7cfc0dd633e4fd6
juhong <juhang720@gmail.com>
2023-05-09 19:51:32 +0900

    create :: notice persistence adapter

--- src/main/kotlin/team/msg/hiv2/domain/notice/application/spi/CommandNoticePort.kt
+++ src/main/kotlin/team/msg/hiv2/domain/notice/application/spi/CommandNoticePort.kt
@@ -1,9 +1,10 @@
 package team.msg.hiv2.domain.notice.application.spi
 
 import team.msg.hiv2.domain.notice.domain.Notice
+import java.util.UUID
 
 interface CommandNoticePort {
     fun saveNotice(notice: Notice)
 
-    fun deleteNotice(notice: Notice)
+    fun deleteNotice(noticeId: UUID)
 }
--- src/main/kotlin/team/msg/hiv2/domain/notice/application/spi/QueryNoticePort.kt
+++ src/main/kotlin/team/msg/hiv2/domain/notice/application/spi/QueryNoticePort.kt
@@ -1,10 +1,11 @@
 package team.msg.hiv2.domain.notice.application.spi
 
+import team.msg.hiv2.domain.notice.domain.Notice
 import java.util.UUID
 
 interface QueryNoticePort {
 
-    fun queryNotice()
+    fun queryNotice(): List<Notice?>
 
-    fun queryNoticeById(noticeId: UUID)
+    fun queryNoticeById(noticeId: UUID): Notice?
 }
