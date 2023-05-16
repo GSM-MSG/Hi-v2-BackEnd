b508620dd4ab250c761bd25cba716ac3313bdf62
aesperer <piyrw9754@gmail.com>
2023-05-16 09:22:20 +0900

    create :: reservation, user  response dto

---
+++ src/main/kotlin/team/msg/hiv2/domain/homebase/application/usecase/QueryReservationUseCase.kt
@@ -0,0 +1,16 @@
+package team.msg.hiv2.domain.homebase.application.usecase
+
+import team.msg.hiv2.domain.homebase.application.spi.QueryHomeBasePort
+import team.msg.hiv2.domain.reservation.application.spi.QueryReservationPort
+import team.msg.hiv2.global.annotation.usecase.ReadOnlyUseCase
+
+@ReadOnlyUseCase
+class QueryReservationUseCase(
+    private val queryReservationPort: QueryReservationPort,
+    private val queryHomeBasePort: QueryHomeBasePort
+) {
+
+    fun execute(floor: Int, period: Int){
+
+    }
+}
---
+++ src/main/kotlin/team/msg/hiv2/domain/reservation/presentation/data/response/ReservationResponse.kt
@@ -0,0 +1,9 @@
+package team.msg.hiv2.domain.reservation.presentation.data.response
+
+import team.msg.hiv2.domain.user.presentation.data.response.UserResponse
+import java.util.*
+
+data class ReservationResponse(
+    val reservationId: UUID,
+    val users: List<UserResponse>
+)
