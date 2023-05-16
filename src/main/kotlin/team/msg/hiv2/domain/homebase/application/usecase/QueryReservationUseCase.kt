57eba6058c41b14d5f1fec6f162fe22efc07aa1b
aesperer <piyrw9754@gmail.com>
2023-05-16 09:24:55 +0900

    create :: query reservation by homebase

--- src/main/kotlin/team/msg/hiv2/domain/homebase/application/usecase/QueryReservationUseCase.kt
+++ src/main/kotlin/team/msg/hiv2/domain/homebase/application/usecase/QueryReservationUseCase.kt
@@ -1,6 +1,7 @@
 package team.msg.hiv2.domain.homebase.application.usecase
 
 import team.msg.hiv2.domain.homebase.application.spi.QueryHomeBasePort
+import team.msg.hiv2.domain.homebase.exception.HomeBaseNotFoundException
 import team.msg.hiv2.domain.reservation.application.spi.QueryReservationPort
 import team.msg.hiv2.global.annotation.usecase.ReadOnlyUseCase
 
@@ -11,6 +12,8 @@ class QueryReservationUseCase(
 ) {
 
     fun execute(floor: Int, period: Int){
+        val homeBase = queryHomeBasePort.queryHomeBaseByFloorAndPeriod(floor, period)
+            .let { it ?: throw HomeBaseNotFoundException() }
 
     }
 }
--- src/main/kotlin/team/msg/hiv2/domain/reservation/persistence/repository/ReservationRepository.kt
+++ src/main/kotlin/team/msg/hiv2/domain/reservation/persistence/repository/ReservationRepository.kt
@@ -1,8 +1,10 @@
 package team.msg.hiv2.domain.reservation.persistence.repository
 
 import org.springframework.data.repository.CrudRepository
+import team.msg.hiv2.domain.homebase.persistence.entity.HomeBaseJpaEntity
 import team.msg.hiv2.domain.reservation.persistence.entity.ReservationJpaEntity
 import java.util.UUID
 
 interface ReservationRepository : CrudRepository<ReservationJpaEntity, UUID> {
+    fun findByHomeBase(homeBase: HomeBaseJpaEntity): ReservationJpaEntity?
 }
