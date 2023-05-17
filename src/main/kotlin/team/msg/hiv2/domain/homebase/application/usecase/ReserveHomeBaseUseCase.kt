603cd6973eeb1d5f353ea18421a25b8448b75c30
aesperer <piyrw9754@gmail.com>
2023-05-17 10:16:33 +0900

    refactor :: 예약 유저 검증 validator 분리

--- src/main/kotlin/team/msg/hiv2/domain/homebase/application/usecase/ReserveHomeBaseUseCase.kt
+++ src/main/kotlin/team/msg/hiv2/domain/homebase/application/usecase/ReserveHomeBaseUseCase.kt
@@ -1,13 +1,12 @@
 package team.msg.hiv2.domain.homebase.application.usecase
 
 import team.msg.hiv2.domain.homebase.application.spi.QueryHomeBasePort
-import team.msg.hiv2.domain.homebase.exception.ForbiddenReserveException
 import team.msg.hiv2.domain.homebase.exception.HomeBaseNotFoundException
 import team.msg.hiv2.domain.homebase.presentation.data.request.ReservationHomeBaseRequest
 import team.msg.hiv2.domain.reservation.application.spi.CommandReservationPort
 import team.msg.hiv2.domain.reservation.domain.Reservation
 import team.msg.hiv2.domain.user.application.spi.UserPort
-import team.msg.hiv2.domain.user.domain.User
+import team.msg.hiv2.domain.user.application.validator.UserValidator
 import team.msg.hiv2.domain.user.domain.constant.UseStatus
 import team.msg.hiv2.global.annotation.usecase.UseCase
 import java.util.*
@@ -16,7 +15,8 @@ import java.util.*
 class ReserveHomeBaseUseCase(
     private val userPort: UserPort,
     private val commandReservationPort: CommandReservationPort,
-    private val queryHomeBasePort: QueryHomeBasePort
+    private val queryHomeBasePort: QueryHomeBasePort,
+    private val userValidator: UserValidator
 ) {
 
     fun execute(floor: Int, period: Int, request: ReservationHomeBaseRequest){
@@ -32,18 +32,10 @@ class ReserveHomeBaseUseCase(
             checkStatus = false
         )
         val users = userPort.queryAllUserById(request.users)
-        checkUseStatus(users)
+
+        userValidator.checkUsersUseStatus(users)
 
         val reservationId = commandReservationPort.saveReservation(reservation).id
         userPort.saveAll(users.map { it.copy(reservationId = reservationId, useStatus = UseStatus.UNAVAILABLE) })
-    }
-
-    private fun checkUseStatus(user: User){
-        if(user.useStatus == UseStatus.UNAVAILABLE)
-            throw ForbiddenReserveException()
-    }
-
-    private fun checkUseStatus(users: List<User>){
-        users.forEach { checkUseStatus(it) }
     }
 }
