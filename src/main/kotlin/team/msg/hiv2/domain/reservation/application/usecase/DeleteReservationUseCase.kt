73ca36e8cc85646be405253c91bd5d18609c785b
aesperer <piyrw9754@gmail.com>
2023-05-18 09:25:03 +0900

    create :: 예약 테이블 삭제 usecase

--- src/main/kotlin/team/msg/hiv2/domain/reservation/application/usecase/DeleteReservationUseCase.kt
+++ src/main/kotlin/team/msg/hiv2/domain/reservation/application/usecase/DeleteReservationUseCase.kt
@@ -1,7 +1,24 @@
 package team.msg.hiv2.domain.reservation.application.usecase
 
+import team.msg.hiv2.domain.reservation.application.spi.ReservationPort
+import team.msg.hiv2.domain.reservation.exception.ReservationNotFoundException
+import team.msg.hiv2.domain.user.application.spi.UserPort
+import team.msg.hiv2.domain.user.domain.constant.UseStatus
 import team.msg.hiv2.global.annotation.usecase.UseCase
+import java.util.UUID
 
 @UseCase
-class DeleteReservationUseCase {
+class DeleteReservationUseCase(
+    private val reservationPort: ReservationPort,
+    private val userPort: UserPort
+) {
+
+    fun execute(reservationId: UUID){
+        val reservation = reservationPort.queryReservationById(reservationId)
+            ?: throw ReservationNotFoundException()
+        val users = userPort.queryAllUserByReservation(reservation)
+
+        userPort.saveAll(users.map { it.copy(useStatus = UseStatus.AVAILABLE) })
+        reservationPort.deleteReservation(reservation)
+    }
 }
