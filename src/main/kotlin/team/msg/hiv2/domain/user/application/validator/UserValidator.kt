8b974c27f6c82454b6bca709d566f95d87d4fa00
aesperer <piyrw9754@gmail.com>
2023-05-23 13:36:20 +0900

    create :: exit reservation usecase

--- src/main/kotlin/team/msg/hiv2/domain/user/application/validator/UserValidator.kt
+++ src/main/kotlin/team/msg/hiv2/domain/user/application/validator/UserValidator.kt
@@ -7,4 +7,6 @@ interface UserValidator {
     fun checkUserUseStatus(user: User)
     fun checkUsersUseStatus(users: List<User>)
     fun checkRepresentative(user: User, reservation: Reservation)
+    fun checkUserAndReservation(user: User, reservation: Reservation)
+
 }
