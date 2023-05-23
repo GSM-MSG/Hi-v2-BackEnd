c264e4d591e7d565b5dc35c42f214d8fa3492efe
JuuuuHong <juhang720@gmail.com>
2023-05-23 16:37:14 +0900

    refactor :: usecase 코드 스타일 맞추기

--- src/main/kotlin/team/msg/hiv2/domain/notice/application/usecase/QueryAllNoticeUseCase.kt
+++ src/main/kotlin/team/msg/hiv2/domain/notice/application/usecase/QueryAllNoticeUseCase.kt
@@ -3,27 +3,29 @@ package team.msg.hiv2.domain.notice.application.usecase
 import team.msg.hiv2.domain.notice.application.spi.QueryNoticePort
 import team.msg.hiv2.domain.notice.presentation.data.response.NoticeResponse
 import team.msg.hiv2.domain.user.application.spi.QueryUserPort
-import team.msg.hiv2.domain.user.exception.UserNotFoundException
 import team.msg.hiv2.domain.user.presentation.data.response.UserResponse
 import team.msg.hiv2.global.annotation.usecase.UseCase
-import java.util.UUID
 
 @UseCase
 class QueryAllNoticeUseCase(
     private val queryNoticePort: QueryNoticePort,
     private val queryUserPort: QueryUserPort
 ) {
-    fun execute(): List<NoticeResponse> =
-        queryNoticePort.queryAllNotice()
-            .map { NoticeResponse(
+    fun execute(): List<NoticeResponse> {
+        val notices = queryNoticePort.queryAllNotice()
+
+        return notices.map {
+            val user = queryUserPort.queryUserByNotice(it.id)
+            NoticeResponse(
+                noticeId = it.id,
                 title = it.title,
                 content = it.content,
-                user = getUser(it.userId)
-            ) }
-
-
-    private fun getUser(id: UUID): UserResponse =
-        queryUserPort.queryUserById(id)
-            .let { it ?: throw UserNotFoundException() }
-            .let { UserResponse(it.id, it.name) }
+                user = UserResponse(
+                    userId = user.id,
+                    name = user.name
+                ),
+                createdAt = it.createdAt
+            )
+        }
+    }
 }
