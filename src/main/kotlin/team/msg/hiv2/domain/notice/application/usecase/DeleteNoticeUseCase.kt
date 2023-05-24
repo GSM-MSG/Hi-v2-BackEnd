e05c518717b227abe973c306e14f0dd8df26434b
JuuuuHong <juhang720@gmail.com>
2023-05-24 13:17:46 +0900

    create :: delete notice usecase

---
+++ src/main/kotlin/team/msg/hiv2/domain/notice/application/usecase/DeleteNoticeUseCase.kt
@@ -0,0 +1,25 @@
+package team.msg.hiv2.domain.notice.application.usecase
+
+import team.msg.hiv2.domain.notice.application.spi.NoticePort
+import team.msg.hiv2.domain.notice.exception.NoticeNotFoundException
+import team.msg.hiv2.domain.user.application.spi.QueryUserPort
+import team.msg.hiv2.domain.user.application.validator.UserValidator
+import team.msg.hiv2.global.annotation.usecase.UseCase
+import java.util.UUID
+
+@UseCase
+class DeleteNoticeUseCase(
+    private val noticePort: NoticePort,
+    private val queryUserPort: QueryUserPort,
+    private val userValidator: UserValidator
+) {
+    fun execute(id: UUID) {
+        val user = queryUserPort.queryCurrentUser()
+        val notice = noticePort.queryNoticeById(id)
+            ?: throw NoticeNotFoundException()
+
+        userValidator.checkUserAndWriter(user.id, notice.userId)
+
+        noticePort.deleteNotice(notice)
+    }
+}
