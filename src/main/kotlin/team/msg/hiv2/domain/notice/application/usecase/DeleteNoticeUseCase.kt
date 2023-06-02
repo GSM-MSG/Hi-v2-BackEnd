93320054f912ba14d08034ae7e62fb024167c13e
JuuuuHong <juhang720@gmail.com>
2023-06-02 12:51:06 +0900

    refactor :: admin은 검증없이 수정 가능

--- src/main/kotlin/team/msg/hiv2/domain/notice/application/usecase/DeleteNoticeUseCase.kt
+++ src/main/kotlin/team/msg/hiv2/domain/notice/application/usecase/DeleteNoticeUseCase.kt
@@ -1,6 +1,7 @@
 package team.msg.hiv2.domain.notice.application.usecase
 
 import team.msg.hiv2.domain.notice.application.spi.NoticePort
+import team.msg.hiv2.domain.notice.exception.ForbiddenCommandNotice
 import team.msg.hiv2.domain.notice.exception.NoticeNotFoundException
 import team.msg.hiv2.domain.user.application.spi.QueryUserPort
 import team.msg.hiv2.domain.user.application.validator.UserValidator
@@ -19,10 +20,8 @@ class DeleteNoticeUseCase(
         val notice = noticePort.queryNoticeById(id)
             ?: throw NoticeNotFoundException()
 
-        if(user.roles.equals(UserRole.ROLE_TEACHER)) {
+        if(user.roles.firstOrNull() == UserRole.ROLE_TEACHER) {
             userValidator.checkUserAndWriter(user.id, notice.userId)
-        } else {
-            userValidator.checkUserAdminRole(user)
         }
 
         noticePort.delete(notice)
