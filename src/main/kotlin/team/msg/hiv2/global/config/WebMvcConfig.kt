960501b8452568099e6bb277a9b29631171d2ee5
aesperer <piyrw9754@gmail.com>
2023-06-03 17:14:11 +0900

    create :: web mvc configuration, cors setting

---
+++ src/main/kotlin/team/msg/hiv2/global/config/WebMvcConfig.kt
@@ -0,0 +1,18 @@
+package team.msg.hiv2.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebMvcConfig : WebMvcConfigurer{
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "HEAD", "DELETE", "OPTIONS")
+            .allowedOrigins(
+                "http://localhost:8080",
+                "https://dev-hi.msg-team.com"
+            )
+    }
+}
