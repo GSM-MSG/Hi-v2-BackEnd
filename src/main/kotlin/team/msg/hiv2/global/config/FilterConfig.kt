62be69c75dd635755d15edf912da2dfa8b7b46a6
aesperer <piyrw9754@gmail.com>
2023-06-12 22:21:39 +0900

    create :: filter config component;

--- src/main/kotlin/team/msg/hiv2/global/config/FilterConfig.kt
+++ src/main/kotlin/team/msg/hiv2/global/config/FilterConfig.kt
@@ -4,10 +4,12 @@ import org.springframework.security.config.annotation.SecurityConfigurerAdapter
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.DefaultSecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.stereotype.Component
 import team.msg.hiv2.global.filter.ExceptionFilter
 import team.msg.hiv2.global.filter.JwtRequestFilter
 import team.msg.hiv2.global.security.spi.JwtParserPort
 
+@Component
 class FilterConfig(
     private val jwtParserPort: JwtParserPort
 ) : SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>() {
