1deebec7bcead8536760a169c9841de00dae5de3
aesperer <piyrw9754@gmail.com>
2023-06-12 22:05:24 +0900

    revert

--- src/main/kotlin/team/msg/hiv2/domain/auth/application/usecase/LogoutUseCase.kt
+++ src/main/kotlin/team/msg/hiv2/domain/auth/application/usecase/LogoutUseCase.kt
@@ -1,7 +1,12 @@
 package team.msg.hiv2.domain.auth.application.usecase
 
+<<<<<<< HEAD
 import team.msg.hiv2.domain.auth.application.service.CommandRefreshTokenService
 import team.msg.hiv2.domain.auth.application.service.QueryRefreshTokenService
+=======
+import team.msg.hiv2.domain.auth.application.spi.RefreshTokenPort
+import team.msg.hiv2.domain.auth.exception.RefreshTokenNotFoundException
+>>>>>>> parent of 6c52165 (refactor :: auth usecase 서비스 분리)
 import team.msg.hiv2.domain.user.application.service.QueryUserService
 import team.msg.hiv2.domain.user.exception.UserNotFoundException
 import team.msg.hiv2.global.annotation.usecase.UseCase
@@ -9,17 +14,30 @@ import team.msg.hiv2.global.annotation.usecase.UseCase
 @UseCase
 class LogoutUseCase(
     private val queryUserService: QueryUserService,
+<<<<<<< HEAD
     private val queryRefreshTokenService: QueryRefreshTokenService,
     private val commandRefreshTokenService: CommandRefreshTokenService
+=======
+    private val refreshTokenPort: RefreshTokenPort
+>>>>>>> parent of 6c52165 (refactor :: auth usecase 서비스 분리)
 ) {
 
     fun execute(refreshToken: String){
         val user = queryUserService.queryCurrentUser()
+<<<<<<< HEAD
         val token = queryRefreshTokenService.queryByRefreshToken(refreshToken)
+=======
+        val token = refreshTokenPort.queryByRefreshToken(refreshToken)
+            ?: throw RefreshTokenNotFoundException()
+>>>>>>> parent of 6c52165 (refactor :: auth usecase 서비스 분리)
 
         if(user.id != token.userId)
             throw UserNotFoundException()
 
+<<<<<<< HEAD
         commandRefreshTokenService.delete(token)
+=======
+        refreshTokenPort.delete(token)
+>>>>>>> parent of 6c52165 (refactor :: auth usecase 서비스 분리)
     }
 }
--- src/main/kotlin/team/msg/hiv2/domain/auth/application/usecase/ReissueTokenUseCase.kt
+++ src/main/kotlin/team/msg/hiv2/domain/auth/application/usecase/ReissueTokenUseCase.kt
@@ -1,17 +1,31 @@
 package team.msg.hiv2.domain.auth.application.usecase
 
+<<<<<<< HEAD
 import team.msg.hiv2.domain.auth.application.service.QueryRefreshTokenService
+=======
+import team.msg.hiv2.domain.auth.application.spi.RefreshTokenPort
+>>>>>>> parent of 6c52165 (refactor :: auth usecase 서비스 분리)
 import team.msg.hiv2.domain.auth.exception.InvalidRefreshTokenException
 import team.msg.hiv2.domain.auth.presentation.data.response.TokenResponse
+<<<<<<< HEAD
 import team.msg.hiv2.domain.user.application.service.QueryUserService
+=======
+import team.msg.hiv2.domain.user.application.spi.UserPort
+import team.msg.hiv2.domain.user.exception.UserNotFoundException
+>>>>>>> parent of 6c52165 (refactor :: auth usecase 서비스 분리)
 import team.msg.hiv2.global.annotation.usecase.UseCase
 import team.msg.hiv2.global.security.spi.GenerateJwtPort
 import team.msg.hiv2.global.security.spi.JwtParserPort
 
 @UseCase
 class ReissueTokenUseCase(
+<<<<<<< HEAD
     private val queryRefreshTokenService: QueryRefreshTokenService,
     private val queryUserService: QueryUserService,
+=======
+    private val refreshTokenPort: RefreshTokenPort,
+    private val userPort: UserPort,
+>>>>>>> parent of 6c52165 (refactor :: auth usecase 서비스 분리)
     private val generateJwtPort: GenerateJwtPort,
     private val jwtParserPort: JwtParserPort
 ) {
@@ -19,9 +33,16 @@ class ReissueTokenUseCase(
     fun execute(requestToken: String): TokenResponse {
         val refreshToken = jwtParserPort.parseRefreshToken(requestToken)
             ?: throw InvalidRefreshTokenException()
+<<<<<<< HEAD
         val token = queryRefreshTokenService.queryByRefreshToken(refreshToken)
         val user = queryUserService.queryUserById(token.userId)
+=======
+>>>>>>> parent of 6c52165 (refactor :: auth usecase 서비스 분리)
 
+        val token = refreshTokenPort.queryByRefreshToken(refreshToken)
+            ?: throw RefreshTokenNotFoundException()
+
+        val user = userPort.queryUserById(token.userId) ?: throw UserNotFoundException()
         return generateJwtPort.generate(user.id, user.roles)
     }
 }
