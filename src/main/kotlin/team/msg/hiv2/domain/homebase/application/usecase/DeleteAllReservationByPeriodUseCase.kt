08f6b238a4d0ac0aee7206d67207297c7a3ddc99
aesperer <piyrw9754@gmail.com>
2023-06-22 10:24:31 +0900

    create :: delete all reservation by period usecase

--- src/main/kotlin/team/msg/hiv2/domain/homebase/application/usecase/DeleteAllReservationByPeriodUseCase.kt
+++ src/main/kotlin/team/msg/hiv2/domain/homebase/application/usecase/DeleteAllReservationByPeriodUseCase.kt
@@ -3,6 +3,7 @@ package team.msg.hiv2.domain.homebase.application.usecase
 import team.msg.hiv2.domain.homebase.application.service.HomeBaseService
 import team.msg.hiv2.domain.reservation.application.service.ReservationService
 import team.msg.hiv2.domain.user.application.service.UserService
+import team.msg.hiv2.domain.user.domain.constant.UseStatus
 import team.msg.hiv2.global.annotation.usecase.UseCase
 
 @UseCase
@@ -15,5 +16,9 @@ class DeleteAllReservationByPeriodUseCase(
     fun execute(period: Int){
         val homeBases = homeBaseService.queryAllHomeBaseByPeriod(period)
         val reservations = reservationService.queryAllReservationByHomeBaseIn(homeBases)
+        val users = userService.queryAllUserByReservationIn(reservations)
+
+        reservationService.deleteAll(reservations)
+        userService.saveAll(users.map { it.copy(useStatus = UseStatus.AVAILABLE) })
     }
 }
