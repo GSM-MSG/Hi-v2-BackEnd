242ff45ab6194bbc24fc5343a4214027e203c583
JuuuuHong <juhang720@gmail.com>
2023-06-22 14:20:36 +0900

    refactor :: Enumrated 사용

--- src/main/kotlin/team/msg/hiv2/domain/user/presentation/data/request/UpdateUserUseStatusRequest.kt
+++ src/main/kotlin/team/msg/hiv2/domain/user/presentation/data/request/UpdateUserUseStatusRequest.kt
@@ -1,9 +1,10 @@
 package team.msg.hiv2.domain.user.presentation.data.request
 
-import com.fasterxml.jackson.annotation.JsonCreator
 import team.msg.hiv2.domain.user.domain.constant.UseStatus
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
 
 class UpdateUserUseStatusRequest(
-    @get:JsonCreator
+    @field:Enumerated(EnumType.STRING)
     val status: UseStatus
 )
