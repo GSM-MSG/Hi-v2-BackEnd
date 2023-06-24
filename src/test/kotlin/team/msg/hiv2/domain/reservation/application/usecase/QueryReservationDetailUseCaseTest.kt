1f9a31ec3b80cdcb2d6433067119bc2e9f282472
aesperer <piyrw9754@gmail.com>
2023-06-24 20:43:53 +0900

    delete :: 내친김에 임포트 삭제하기

--- src/test/kotlin/team/msg/hiv2/domain/reservation/application/usecase/QueryReservationDetailUseCaseTest.kt
+++ src/test/kotlin/team/msg/hiv2/domain/reservation/application/usecase/QueryReservationDetailUseCaseTest.kt
@@ -1,6 +1,5 @@
 package team.msg.hiv2.domain.reservation.application.usecase
 
-import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
