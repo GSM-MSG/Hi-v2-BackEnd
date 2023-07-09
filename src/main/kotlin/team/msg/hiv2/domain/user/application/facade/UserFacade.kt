68ace836da38f649f6c8427d5c6a2d25a603a284
aesperer <piyrw9754@gmail.com>
2023-07-09 17:59:47 +0900

    create :: user facade impl

---
+++ src/main/kotlin/team/msg/hiv2/domain/user/application/facade/UserFacade.kt
@@ -0,0 +1,15 @@
+package team.msg.hiv2.domain.user.application.facade
+
+import team.msg.hiv2.domain.user.presentation.data.request.SearchUserKeywordRequest
+import team.msg.hiv2.domain.user.presentation.data.request.UpdateUserUseStatusRequest
+import team.msg.hiv2.domain.user.presentation.data.response.AllStudentsResponse
+import team.msg.hiv2.domain.user.presentation.data.response.UserInfoResponse
+import team.msg.hiv2.domain.user.presentation.data.response.UserResponse
+import java.util.*
+
+interface UserFacade {
+    fun queryAllStudents(): AllStudentsResponse
+    fun queryUserInfo(): UserInfoResponse
+    fun searchUserByNameKeyword(request: SearchUserKeywordRequest): List<UserResponse>
+    fun updateUserUseStatus(userId: UUID, request: UpdateUserUseStatusRequest)
+}
