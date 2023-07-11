dd796c67a4e5015e4fd370de7bbd641dd81769e0
aesperer <piyrw9754@gmail.com>
2023-07-11 20:04:32 +0900

    delete :: import assertions;

--- src/test/kotlin/team/msg/hiv2/domain/reservation/application/usecase/DelegateRepresentativeUseCaseTest.kt
+++ src/test/kotlin/team/msg/hiv2/domain/reservation/application/usecase/DelegateRepresentativeUseCaseTest.kt
@@ -1,6 +1,5 @@
 package team.msg.hiv2.domain.reservation.application.usecase
 
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
