23e2aec9ca3b49ee9a979c6e2a890cfb212fceba
aesperer <piyrw9754@gmail.com>
2023-08-10 23:19:25 +0900

    chore :: health check adapter 패키지 이동

--- src/main/kotlin/team/msg/hiv2/domain/health/HealthCheckWebAdapter.kt
+++ src/main/kotlin/team/msg/hiv2/domain/health/HealthCheckWebAdapter.kt
@@ -1,4 +1,4 @@
-package team.msg.hiv2.domain
+package team.msg.hiv2.domain.health
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
