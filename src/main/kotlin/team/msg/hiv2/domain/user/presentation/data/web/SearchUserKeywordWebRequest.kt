7cdae2a92e9ec5b3a39445785991214100fe98b7
aesperer <piyrw9754@gmail.com>
2023-08-10 22:19:46 +0900

    refactor :: keyword to query string

--- src/main/kotlin/team/msg/hiv2/domain/user/presentation/data/web/SearchUserKeywordWebRequest.kt
+++
@@ -1,8 +0,0 @@
-package team.msg.hiv2.domain.user.presentation.data.web
-
-import javax.validation.constraints.NotNull
-
-data class SearchUserKeywordWebRequest(
-    @field:NotNull
-    val keyword: String
-)
