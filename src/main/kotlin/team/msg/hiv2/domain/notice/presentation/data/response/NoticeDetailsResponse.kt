e98f221da4cc95e19df359cdc68deab506689d9a
JuuuuHong <juhang720@gmail.com>
2023-08-20 17:48:25 +0900

    feat :: notice details response of 함수

--- src/main/kotlin/team/msg/hiv2/domain/notice/presentation/data/response/NoticeDetailsResponse.kt
+++ src/main/kotlin/team/msg/hiv2/domain/notice/presentation/data/response/NoticeDetailsResponse.kt
@@ -1,5 +1,6 @@
 package team.msg.hiv2.domain.notice.presentation.data.response
 
+import team.msg.hiv2.domain.notice.domain.Notice
 import team.msg.hiv2.domain.user.presentation.data.response.UserResponse
 import java.time.LocalDateTime
 import java.util.*
@@ -10,4 +11,14 @@ data class NoticeDetailsResponse (
     val content: String,
     val user: UserResponse,
     val createdAt: LocalDateTime
-)
+) {
+    companion object {
+        fun of(notice: Notice, user: UserResponse) = NoticeDetailsResponse(
+            noticeId = notice.id,
+            title = notice.title,
+            content = notice.content,
+            user = user,
+            createdAt = notice.createdAt
+        )
+    }
+}
