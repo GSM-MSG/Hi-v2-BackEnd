69c00299cc0460fd95f7ab4779c2a1c5faae7d3d
JuuuuHong <juhang720@gmail.com>
2023-08-25 08:34:16 +0900

    refactor :: data 클래스, validation

--- src/main/kotlin/team/msg/hiv2/domain/user/presentation/data/request/UpdateUserRoleWebRequest.kt
+++ src/main/kotlin/team/msg/hiv2/domain/user/presentation/data/request/UpdateUserRoleWebRequest.kt
@@ -1,7 +1,9 @@
 package team.msg.hiv2.domain.user.presentation.data.request
 
+import javax.validation.constraints.NotNull
 import team.msg.hiv2.domain.user.domain.constant.UserRole
 
-class UpdateUserRoleWebRequest(
+data class UpdateUserRoleWebRequest(
+    @NotNull
     val role: UserRole
 )
