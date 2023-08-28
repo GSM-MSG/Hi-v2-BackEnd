4076b86a3b2f31e43e9bfb6b676b34ba7f5b0954
JuuuuHong <juhang720@gmail.com>
2023-08-28 19:29:27 +0900

    feat :: notice writer aspect

---
+++ src/main/kotlin/team/msg/hiv2/aspect/NoticeAspect.kt
@@ -0,0 +1,37 @@
+package team.msg.hiv2.aspect
+
+import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.annotation.Before
+import org.aspectj.lang.annotation.Pointcut
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+import team.msg.hiv2.domain.notice.application.service.NoticeService
+import team.msg.hiv2.domain.notice.presentation.data.request.UpdateNoticeRequest
+import team.msg.hiv2.domain.user.application.service.UserService
+import team.msg.hiv2.domain.user.application.validator.UserValidator
+import team.msg.hiv2.domain.user.domain.constant.UserRole
+import java.util.*
+
+@Component
+@Aspect
+class NoticeAspect(
+    private val userService: UserService,
+    private val noticeService: NoticeService,
+    private val userValidator: UserValidator
+) {
+
+    private val log by lazy { LoggerFactory.getLogger(this.javaClass.simpleName) }
+
+    @Pointcut("execution(* team.msg.hiv2.domain.notice.application.usecase.UpdateNoticeUseCase.execute(..))" +
+            "&& args(id, updateNoticeUseCase) && within(team.msg.hiv2.domain.notice.application.usecase.UpdateNoticeUseCase)")
+    private fun updateNoticeUseCasePointcut(id: UUID,updateNoticeRequest: UpdateNoticeRequest) {}
+
+    @Before("updateNoticeUseCasePointcut(id, updateNoticeUseCase(id, updateNoticeUseCase))")
+    private fun checkWriter(id: UUID, updateNoticeRequest: UpdateNoticeRequest) {
+        val user = userService.queryCurrentUser()
+        val notice = noticeService.queryNoticeById(id)
+
+        if (user.roles.firstOrNull() == UserRole.ROLE_TEACHER)
+            userValidator.checkUserAndWriter(user.id, notice.userId)
+    }
+}
