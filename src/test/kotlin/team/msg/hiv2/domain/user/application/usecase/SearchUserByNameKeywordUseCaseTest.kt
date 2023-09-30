e801fcfcbb77f742f6e70eef6303f270935a0130
aesperer <piyrw9754@gmail.com>
2023-09-30 16:35:41 +0900

    update :: search user test 모킹 수정

--- src/test/kotlin/team/msg/hiv2/domain/user/application/usecase/SearchUserByNameKeywordUseCaseTest.kt
+++ src/test/kotlin/team/msg/hiv2/domain/user/application/usecase/SearchUserByNameKeywordUseCaseTest.kt
@@ -64,7 +64,7 @@ class SearchUserByNameKeywordUseCaseTest {
     @Test
     fun `유저 검색 성공`() {
         // given
-        given(userService.queryUserByNameContaining(requestStub.keyword))
+        given(userService.queryUserByNameContainingOrderByEmail(requestStub.keyword))
             .willReturn(listOf(userStub))
 
         // when
