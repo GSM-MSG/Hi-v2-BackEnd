2276d304fe8df9ad56c1ebc60b1d9d7b0ab3fa02
JuuuuHong <juhang720@gmail.com>
2023-10-17 08:44:02 +0900

    update :: query all users by role

--- src/main/kotlin/team/msg/hiv2/domain/user/application/facade/UserFacadeImpl.kt
+++ src/main/kotlin/team/msg/hiv2/domain/user/application/facade/UserFacadeImpl.kt
@@ -1,17 +1,18 @@
 package team.msg.hiv2.domain.user.application.facade
 
 import org.springframework.stereotype.Component
-import team.msg.hiv2.domain.user.application.usecase.QueryAllStudentsUseCase
+import team.msg.hiv2.domain.user.application.usecase.QueryAllUsersByUserRoleUseCase
 import team.msg.hiv2.domain.user.application.usecase.QueryMyRoleUseCase
 import team.msg.hiv2.domain.user.application.usecase.QueryUserInfoUseCase
 import team.msg.hiv2.domain.user.application.usecase.SearchStudentByNameKeywordUseCase
 import team.msg.hiv2.domain.user.application.usecase.SearchUserByNameKeywordUseCase
 import team.msg.hiv2.domain.user.application.usecase.UpdateUserRoleUseCase
 import team.msg.hiv2.domain.user.application.usecase.UpdateUserUseStatusUseCase
+import team.msg.hiv2.domain.user.domain.constant.UserRole
 import team.msg.hiv2.domain.user.presentation.data.request.SearchUserKeywordRequest
 import team.msg.hiv2.domain.user.presentation.data.request.UpdateUserRoleWebRequest
 import team.msg.hiv2.domain.user.presentation.data.request.UpdateUserUseStatusRequest
-import team.msg.hiv2.domain.user.presentation.data.response.AllStudentsResponse
+import team.msg.hiv2.domain.user.presentation.data.response.AllUsersResponse
 import team.msg.hiv2.domain.user.presentation.data.response.StudentResponse
 import team.msg.hiv2.domain.user.presentation.data.response.UserInfoResponse
 import team.msg.hiv2.domain.user.presentation.data.response.UserResponse
@@ -20,7 +21,7 @@ import java.util.*
 
 @Component
 class UserFacadeImpl(
-    private val queryAllStudentsUseCase: QueryAllStudentsUseCase,
+    private val queryAllUsersByUserRoleUseCase: QueryAllUsersByUserRoleUseCase,
     private val queryUserInfoUseCase: QueryUserInfoUseCase,
     private val queryMyRoleUseCase: QueryMyRoleUseCase,
     private val searchUserByNameKeywordUseCase: SearchUserByNameKeywordUseCase,
@@ -29,8 +30,8 @@ class UserFacadeImpl(
     private val searchStudentByNameKeywordUseCase: SearchStudentByNameKeywordUseCase
 ) : UserFacade {
 
-    override fun queryAllStudents(): AllStudentsResponse =
-        queryAllStudentsUseCase.execute()
+    override fun queryAllUsersByUserRole(userRole: UserRole): AllUsersResponse =
+        queryAllUsersByUserRoleUseCase.execute(userRole)
 
     override fun queryUserInfo(): UserInfoResponse =
         queryUserInfoUseCase.execute()
--- src/main/kotlin/team/msg/hiv2/domain/user/presentation/data/response/AllStudentsResponse.kt
+++
@@ -1,5 +0,0 @@
-package team.msg.hiv2.domain.user.presentation.data.response
-
-data class AllStudentsResponse(
-    val student: List<StudentResponse>
-)
