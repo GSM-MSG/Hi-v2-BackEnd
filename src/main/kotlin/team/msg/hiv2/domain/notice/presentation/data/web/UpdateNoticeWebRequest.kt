652e5175c325416402af6c089eececbb57f045a8
JuuuuHong <juhang720@gmail.com>
2024-03-05 08:02:40 +0900

    add :: notice string size 제한

--- src/main/kotlin/team/msg/hiv2/domain/notice/presentation/data/web/UpdateNoticeWebRequest.kt
+++ src/main/kotlin/team/msg/hiv2/domain/notice/presentation/data/web/UpdateNoticeWebRequest.kt
@@ -1,10 +1,13 @@
 package team.msg.hiv2.domain.notice.presentation.data.web
 
 import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.Size
 
 data class UpdateNoticeWebRequest(
     @field:NotEmpty
+    @field:Size(max = 20)
     val title: String,
     @field:NotEmpty
+    @field:Size(max = 200)
     val content: String
 )
