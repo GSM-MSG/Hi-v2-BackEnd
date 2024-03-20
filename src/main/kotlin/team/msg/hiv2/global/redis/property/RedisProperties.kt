efd813fa6e45ec7109049c45c92fbf57f8224dc5
JuuuuHong <juhang720@gmail.com>
2024-03-20 16:49:13 +0900

    update :: configuration properties

--- src/main/kotlin/team/msg/hiv2/global/redis/property/RedisProperties.kt
+++ src/main/kotlin/team/msg/hiv2/global/redis/property/RedisProperties.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
 
 @ConstructorBinding
-@ConfigurationProperties("redis")
+@ConfigurationProperties("spring.redis")
 class RedisProperties(
     val host: String,
     val port: Int
