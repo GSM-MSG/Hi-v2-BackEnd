b641b2cb1b9f045724c754c72697947d86a7f12d
JuuuuHong <juhang720@gmail.com>
2024-03-21 15:04:38 +0900

    update :: update reservation usecase 예외처리 순서 변경

--- src/main/kotlin/team/msg/hiv2/domain/reservation/application/usecase/UpdateReservationUseCase.kt
+++ src/main/kotlin/team/msg/hiv2/domain/reservation/application/usecase/UpdateReservationUseCase.kt
@@ -2,10 +2,12 @@ package team.msg.hiv2.domain.reservation.application.usecase
 
 import team.msg.hiv2.domain.homebase.application.service.HomeBaseService
 import team.msg.hiv2.domain.homebase.exception.AlreadyExistReservationException
+import team.msg.hiv2.domain.homebase.exception.TooManyUsersException
 import team.msg.hiv2.domain.reservation.application.service.ReservationService
 import team.msg.hiv2.domain.reservation.presentation.data.request.UpdateReservationRequest
 import team.msg.hiv2.domain.team.application.service.TeamService
 import team.msg.hiv2.domain.user.application.service.UserService
+import team.msg.hiv2.domain.user.exception.UserNotFoundException
 import team.msg.hiv2.global.annotation.usecase.UseCase
 import java.util.*
 
@@ -21,6 +23,11 @@ class UpdateReservationUseCase(
 
         val reservation = reservationService.queryReservationById(reservationId)
 
+        val homeBase = homeBaseService.queryHomeBaseById(reservation.homeBaseId)
+
+        if (request.users.size > homeBase.maxCapacity)
+            throw TooManyUsersException()
+
         val homeBases = homeBaseService.queryHomeBaseById(reservation.homeBaseId).let { homeBaseService.queryHomeBaseByPeriod(it.period) }
         val teamIds = reservationService.queryAllReservationByHomeBaseIn(homeBases).map { reservation -> reservation.teamId }
         val userIds = teamService.queryAllTeamByIdIn(teamIds).flatMap { team -> team.userIds }
@@ -31,6 +38,10 @@ class UpdateReservationUseCase(
         val team = teamService.queryTeamById(reservation.teamId)
 
         val users = userService.queryAllUserById(request.users)
+
+        if (request.users.size != users.size)
+            throw UserNotFoundException()
+
 
         teamService.save(team.copy(userIds = users.map { it.id }.toMutableList()))
         reservationService.save(reservation.copy(reason = request.reason))
