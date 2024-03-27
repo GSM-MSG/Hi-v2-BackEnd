f98bfdb90d480a7631d963efcaa7aa075d480fa7
JuuuuHong <juhang720@gmail.com>
2024-03-27 21:08:53 +0900

    add :: redis property

---
+++ src/main/kotlin/team/msg/hiv2/global/redis/RedisProperties.kt
@@ -0,0 +1,15 @@
+package team.msg.hiv2.global.redis
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+
+@ConstructorBinding
+@ConfigurationProperties("spring.redis")
+class RedisProperties(
+    val host: String,
+    val port: Int
+) {
+    companion object {
+        const val REDISSON_HOST_PREFIX = "redis://"
+    }
+}
