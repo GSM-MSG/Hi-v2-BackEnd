61c585ba6e09d12a3b51218ae730b5cd09de64b7
JuuuuHong <juhang720@gmail.com>
2024-06-14 10:18:33 +0900

    delete :: ConstructorBinding annotation

--- src/main/kotlin/team/msg/hiv2/global/security/token/JwtProperties.kt
+++ src/main/kotlin/team/msg/hiv2/global/security/token/JwtProperties.kt
@@ -2,11 +2,9 @@ package team.msg.hiv2.global.security.token
 
 import io.jsonwebtoken.security.Keys
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.ConstructorBinding
 import java.nio.charset.StandardCharsets
 import java.security.Key
 
-@ConstructorBinding
 @ConfigurationProperties(prefix = "jwt")
 class JwtProperties(
     accessSecret: String,
