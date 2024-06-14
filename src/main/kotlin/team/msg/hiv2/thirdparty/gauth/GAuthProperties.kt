c85c17d2e943dc33d89fe1ac018e27ff4b41601b
JuuuuHong <juhang720@gmail.com>
2024-06-14 10:18:03 +0900

    delete :: ConstructorBinding annotation

--- src/main/kotlin/team/msg/hiv2/thirdparty/gauth/GAuthProperties.kt
+++ src/main/kotlin/team/msg/hiv2/thirdparty/gauth/GAuthProperties.kt
@@ -1,9 +1,7 @@
 package team.msg.hiv2.thirdparty.gauth
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.ConstructorBinding
 
-@ConstructorBinding
 @ConfigurationProperties(prefix = "gauth")
 data class GAuthProperties(
     val clientId: String,
