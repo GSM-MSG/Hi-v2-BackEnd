d09f7ec9b131b25af3ef1c4cbbd4aa4763bb7989
JuuuuHong <juhang720@gmail.com>
2024-06-14 09:17:21 +0900

    update :: spring boot version up to 3.0

--- buildSrc/src/main/kotlin/PluginVersion.kt
+++ buildSrc/src/main/kotlin/PluginVersion.kt
@@ -1,6 +1,6 @@
 object PluginVersion {
-    const val SPRING_BOOT_VERSION = "2.7.3"
-    const val DEPENDENCY_MANAGER_VERSION = "1.0.13.RELEASE"
+    const val SPRING_BOOT_VERSION = "3.1.5"
+    const val DEPENDENCY_MANAGER_VERSION = "1.1.13"
     const val JVM_VERSION = "1.7.10"
     const val SPRING_PLUGIN_VERSION = "1.7.10"
     const val JPA_PLUGIN_VERSION = "1.7.10"
