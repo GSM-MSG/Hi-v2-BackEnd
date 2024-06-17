b727f0b11dd70e6ec673e721d1056e973a1f719c
JuuuuHong <juhang720@gmail.com>
2024-06-17 09:21:06 +0900

    update :: deprecated annotation

--- src/main/kotlin/team/msg/hiv2/global/entity/BaseUuidEntity.kt
+++ src/main/kotlin/team/msg/hiv2/global/entity/BaseUuidEntity.kt
@@ -1,18 +1,13 @@
 package team.msg.hiv2.global.entity
 
-import org.hibernate.annotations.GenericGenerator
-import java.util.UUID
-import jakarta.persistence.Column
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.Id
-import jakarta.persistence.MappedSuperclass
+import jakarta.persistence.*
+import java.util.*
 
 @MappedSuperclass
 abstract class BaseUuidEntity(
 
     @Id
-    @GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @GeneratedValue(generator = "uuid2", strategy = GenerationType.UUID)
     @Column(columnDefinition = "BINARY(16)", nullable = false)
     open val id: UUID
 ) : BaseTimeEntity()
