f42ac943bb258d7d50c6688a7a1e44531f0300f2
JuuuuHong <juhang720@gmail.com>
2024-06-17 17:25:27 +0900

    update :: hibernate 6

--- src/main/kotlin/team/msg/hiv2/global/entity/BaseUuidEntity.kt
+++ src/main/kotlin/team/msg/hiv2/global/entity/BaseUuidEntity.kt
@@ -1,13 +1,18 @@
 package team.msg.hiv2.global.entity
 
 import jakarta.persistence.*
+import org.hibernate.annotations.GenericGenerator
+import org.hibernate.annotations.JdbcTypeCode
+import org.hibernate.type.SqlTypes
 import java.util.*
 
 @MappedSuperclass
 abstract class BaseUuidEntity(
 
     @Id
-    @GeneratedValue(generator = "uuid2", strategy = GenerationType.UUID)
+    @GeneratedValue(generator = "uuid2")
     @Column(columnDefinition = "BINARY(16)", nullable = false)
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    @JdbcTypeCode(SqlTypes.VARCHAR)
     open val id: UUID
 ) : BaseTimeEntity()
